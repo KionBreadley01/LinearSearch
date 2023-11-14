75019418d9209d43057828cecb6fa645856f717b
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-11-13 20:15:05 -0600

    03 establecer el funcionamiento del codigo

--- src/linearsearch/LinearSearch.java
+++ src/linearsearch/LinearSearch.java
@@ -14,22 +14,30 @@ public class LinearSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    // definir el arrglo, con un conjunto de números
-    int array[] = {7, 12, 3, 9, 14, 6, 8, 1, 11, 5, 13, 2, 10, 4, 15};
-    // Establecer el nuemro que se va a encontrar en la lista 
-    int target = 15;
+        // Definir el arreglo con un conjunto de números
+        int array[] = {7, 12, 3, 9, 14, 6, 8, 1, 11, 5, 13, 2, 10, 4, 15};
+        
+        // Establecer el número que se va a encontrar en el arreglo
+        int target = 15;
    
-    int result = linearSearch(array, target);
-    
-    if(result != -1){
-        System.out.println("El numero" + target+ "se encuentra en la posion " + result);
-    }else{
-        System.out.println("El elemento" + target + "no se encontro en numero  en la lista");
+        int result = linearSearch(array, target);
+        
+        if(result != -1){
+            System.out.println("El número " + target + " se encuentra en la posición " + result);
+        } else {
+            System.out.println("El elemento " + target + " no se encontró en el arreglo.");
         }
     }
 
     private static int linearSearch(int[] array, int target) {
-        
+        // Recorrer el arreglo para buscar el número
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                // Devolver la posición del número si se encuentra
+                return i;
+            }
+        }
+        // Devolver -1 si el número no se encuentra en el arreglo
+        return -1;
     }
-    
 }
