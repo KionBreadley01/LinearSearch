f33db09b02c488ddad48dd60fb909bc47c730ef3
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-11-13 19:19:52 -0600

    02 Linear Search

--- src/linearsearch/LinearSearch.java
+++ src/linearsearch/LinearSearch.java
@@ -6,7 +6,7 @@ package linearsearch;
 
 /**
  *
- * @author ROBERTO
+ * @author juan 
  */
 public class LinearSearch {
 
@@ -14,7 +14,22 @@ public class LinearSearch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    // definir el arrglo, con un conjunto de números
+    int array[] = {7, 12, 3, 9, 14, 6, 8, 1, 11, 5, 13, 2, 10, 4, 15};
+    // Establecer el nuemro que se va a encontrar en la lista 
+    int target = 15;
+   
+    int result = linearSearch(array, target);
+    
+    if(result != -1){
+        System.out.println("El numero" + target+ "se encuentra en la posion " + result);
+    }else{
+        System.out.println("El elemento" + target + "no se encontro en numero  en la lista");
+        }
+    }
+
+    private static int linearSearch(int[] array, int target) {
+        
     }
     
 }
